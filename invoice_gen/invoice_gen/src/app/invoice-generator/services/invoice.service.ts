655d525a6a66583cbd64f455d6850ae8b30edc99
AnmeshRapol <anmesh.rapol.5@gmail.com>
2024-06-19 17:45:43 +0530

    added filter for admin panel

--- invoice_gen/invoice_gen/src/app/invoice-generator/services/invoice.service.ts
+++ invoice_gen/invoice_gen/src/app/invoice-generator/services/invoice.service.ts
@@ -59,6 +59,13 @@ export class InvoiceService {
     return this._http.post<any>(`${this.invoiceUrl}/invoiceData`, invoice);
   }
 
+
+  getAllInvoiceDataByStatusId(setdays:number): Observable<Invoicedetails[]> {
+    // alert(this.getAllEmployees)
+    // Send a GET request to the API to retrieve a list of all employees
+    return this._http.get<Invoicedetails[]>(`${this.invoiceUrl}/invoiceProgress/${setdays}`);
+  }
+
 }
 
 
