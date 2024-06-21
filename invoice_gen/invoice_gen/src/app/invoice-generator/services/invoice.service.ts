4a2a6a41969dcbe4e168034f13d1574f1c205200
Kaushik <kaushikwalke6565@gmail.com>
2024-06-21 14:25:15 +0530

    table align

--- invoice_gen/invoice_gen/src/app/invoice-generator/services/invoice.service.ts
+++ invoice_gen/invoice_gen/src/app/invoice-generator/services/invoice.service.ts
@@ -11,8 +11,8 @@ import { invoicegen } from '../components/class/invoicegen';
 export class InvoiceService {
 
 
-  // private invoiceUrl: string = `http://localhost:8090/invoice/excel`;
-  private invoiceUrl: string = `http://localhost:8090/excel`;
+  private invoiceUrl: string = `http://localhost:8090/invoice/excel`;
+  // private invoiceUrl: string = `http://localhost:8090/excel`;
   private countryUrl: any;
   private companyUrl: any;
 
