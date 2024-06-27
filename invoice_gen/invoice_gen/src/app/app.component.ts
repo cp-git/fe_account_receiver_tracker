5daf70ab96294a87a2b6f9c8010fe6bcaabe3cb3
Kaushik <kaushikwalke6565@gmail.com>
2024-06-27 13:43:26 +0530

    commit auto logout

--- invoice_gen/invoice_gen/src/app/app.component.ts
+++ invoice_gen/invoice_gen/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
+import { AutologoutService } from './login/authservice/autologout.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,14 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
 
+  constructor(private authService: AutologoutService) { }
+
+  // Listen for user activity events
+  @HostListener('window:mousemove') onMouseMove() {
+    this.authService.simulateUserActivity();
+  }
+
+  @HostListener('window:keypress') onKeyPress() {
+    this.authService.simulateUserActivity();
+  }
 }
